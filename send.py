56924637b7418c9108c96f6a1fd5ca7f8f99c103
misterch0c <misterch0c@users.noreply.github.com>
2019-12-06 08:33:56 +0100

    Update send.py
    oopsie

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ import sys
 def telegram_bot_sendtext(bot_message):
     bot_token = ''
     bot_chatID = ''
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_me$
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
     response = requests.get(send_text)
 
